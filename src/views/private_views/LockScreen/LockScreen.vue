cbb82e7f96e642a057c6894406f9da23fcfc1df8
unknown <frsummit1@gmail.com>
2021-02-11 11:28:47 +0600

    lock screen style fixed

--- src/views/private_views/LockScreen/LockScreen.vue
+++ src/views/private_views/LockScreen/LockScreen.vue
@@ -13,41 +13,39 @@
               </p>
             </div>
             <div class="pin-input-section">
-                <div class="pin-input-section-inner">
-                    <div class="label">
-                        <div class="label-inner">
-                            <span>Enter PIN</span>
-                        </div>
-                    </div>
-                    <div class="input-sec">
-                        <div class="input-sec-inner">
-                            <div class="blmd-line">
-                                <input
-                                type="password"
-                                v-model="password"
-                                name="password"
-                                autocomplete="off"
-                                id="password"
-                                class="form-control"
-                                />
-                                <label class="blmd-label">Password</label>
-                                <!-- <img class="blmd-icon" src="../../../assets/icons/pass_icon.svg" alt="key"> -->
-                                <div class="password-show-hide hide" id="password-show-hide" @click="passwordShowHide">
-                                    <div v-show="showPass"><i class="fas fa-eye"></i></div>
-                                    <div v-show="!showPass"><i class="fas fa-eye-slash"></i></div>
-                                </div>
-                            </div>
-                            <div class="submit-section">
-                                <span><i class="zmdi zmdi-arrow-right"></i></span>
-                            </div>
-                        </div>
-                    </div>
+              <div class="pin-input-section-inner">
+                <div class="label">
+                  <div class="label-inner">
+                    <span>Enter PIN</span>
+                  </div>
                 </div>
+                <div class="input-sec">
+                  <div class="input-sec-inner">
+                    <div class="blmd-line">
+                      <input
+                        type="text"
+                        v-model="pin"
+                        name="pin"
+                        autocomplete="off"
+                        id="pin"
+                        class="form-control"
+                      />
+                      <div class="pin-show" @click="passwordShowHide">
+                        <div v-show="showPass"><i class="fas fa-eye"></i></div>
+                        <div v-show="!showPass"><i class="fas fa-eye-slash"></i></div>
+                      </div>
+                    </div>
+                    <div class="submit-section">
+                      <span><i class="zmdi zmdi-arrow-right"></i></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
             <div class="help-section">
-                <div class="help-section-inner">
-                    <span>Need Help?</span>
-                </div>
+              <div class="help-section-inner">
+                <span>Need Help?</span>
+              </div>
             </div>
           </div>
         </div>
@@ -59,7 +57,10 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      pin: "",
+      showPass: false,
+    };
   },
   created() {
     this.$emit("routeName", this.$route.name);
