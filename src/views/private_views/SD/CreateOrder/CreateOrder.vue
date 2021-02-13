50891b098f08685c7efd10eb59b0aff8ea60d48c
unknown <frsummit1@gmail.com>
2021-02-13 15:43:06 +0600

    Create Order header create

--- src/views/private_views/SD/CreateOrder/CreateOrder.vue
+++ src/views/private_views/SD/CreateOrder/CreateOrder.vue
@@ -1,5 +1,38 @@
 <template>
-    <div id="create-order" class="create-order">
-        Create Order
-    </div>
+  <div id="create-order" class="create-order">
+    <Heading :pathName="pathName" :routeName="routeName" />
+  </div>
 </template>
+
+<script>
+import Heading from "../../../../components/master_layout/HeadingTitleBreadcrumb/HeadingTitleBreadcrumb";
+import BreadcrumbFunctions from '../../../../functions/BreadcrumbFunctions'
+const breadcrumbFunctions = new BreadcrumbFunctions()
+
+export default {
+  components: {
+    Heading,
+  },
+  data() {
+    return {
+      routeName: "Create Order",
+      parentPath: "Local Sales",
+      pathName: [],
+    };
+  },
+  created() {
+    this.$emit("routeName", this.$route.name);
+    this.createBreadcrumbData();
+  },
+  mounted() {},
+  methods: {
+    createBreadcrumbData() {
+      this.pathName = breadcrumbFunctions.jmiERPBreadcrumb(window.location.pathname);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import url("./CreateOrder.less");
+</style>
