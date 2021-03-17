7f34a95cd0bcae4b7406288b72428103c8a97a32
unknown <frsummit1@gmail.com>
2021-03-17 11:28:51 +0600

    invoice challan summery page ready, style fixed

---
+++ src/views/private_views/SD/InvoiceChallanSummery/InvoiceChallanSummery.vue
@@ -0,0 +1,105 @@
+<template>
+  <div id="invoice-challan-summery" class="invoice-challan-summery">
+    <Heading :pathName="pathName" :routeName="routeName" />
+    <div class="invoice-challan-summery-section">
+      <div class="invoice-challan-summery-inner">
+        <InvoiceChallanPrintingLeftList
+          v-on:invoice_id_from_left="invoiceIdFromLeftHandler"/>
+        <div class="invoice-challan-summery-detail-section">
+          <div class="invoice-challan-summery-detail-inner">
+            <DetailSection 
+              :SCHEDULE_DETAILS_LIST="SCHEDULE_DETAILS_LIST"
+              :SCHEDULE_DETAILS_LIST_CHEMIST="SCHEDULE_DETAILS_LIST_CHEMIST"
+              :SCHEDULE_DETAILS_LIST_INSTITUTION="SCHEDULE_DETAILS_LIST_INSTITUTION" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Order Approved Message -->
+    <!-- <div id="info-modal" class="modal-popup-section info-modal" v-if="info_modal_schedult_count">
+      <div class="modal-popup-section-inner update-successfully-modal-inner">
+        <span class="proceed-popup-icon"><i class="zmdi zmdi-check-circle"></i></span>
+        <p class="popup-text info">Total Invoice {{ schedule_count }}</p>
+      </div>
+    </div> -->
+  </div>
+</template>
+
+<script>
+import Heading from "../../../../components/master_layout/HeadingTitleBreadcrumb/HeadingTitleBreadcrumb";
+import InvoiceChallanPrintingLeftList from "./Sidebar/InvoiceChallanPrintingLeftList";
+import DetailSection from "./DetailSection/DetailSection";
+// import BreadcrumbFunctions from '../../../../functions/BreadcrumbFunctions'
+// const breadcrumbFunctions = new BreadcrumbFunctions()
+import Service from "../../../../service/ERPSidebarService";
+const service = new Service();
+
+export default {
+  components: {
+    Heading,
+    InvoiceChallanPrintingLeftList,
+    DetailSection,
+  },
+  data() {
+    return {
+      routeName: "Delivery Process Invoice/Challan Printing",
+      parentPath: "Local Sales",
+      pathName: [],
+      // info_modal_schedult_count: false,
+      schedule_count: null,
+      SCHEDULE_DETAILS_LIST: [],
+      SCHEDULE_DETAILS_LIST_CHEMIST: [],
+      SCHEDULE_DETAILS_LIST_INSTITUTION: [],
+    };
+  },
+  created() {
+    this.$emit("routeName", this.$route.name);
+    this.createBreadcrumbData();
+  },
+  mounted() {
+    // this.showInvoiceCountInformationMessagePopup()
+  },
+  methods: {
+    createBreadcrumbData() {
+      this.pathName = [{name: "Features"},{ name: "Local Sales" }, { name: "Delivery Process Invoice/Challan Printing" }];
+      // this.pathName = breadcrumbFunctions.jmiERPBreadcrumb(window.location.pathname)
+    },
+    /*showInvoiceCountInformationMessagePopup() {
+      this.schedule_count = this.$route.path.split(':')[1]
+      this.info_modal_schedult_count = true
+      setTimeout( () => {
+        this.info_modal_schedult_count = false
+      }, 2000)
+    },*/
+    async invoiceIdFromLeftHandler(val) {
+      console.log(val)
+      await this.DELIVERY_SCHEDULE_DETAILS__FROM_SERVICE(val)
+    },
+    // -------------------------------------------------------------------------------
+    // Service Call
+    async DELIVERY_SCHEDULE_DETAILS__FROM_SERVICE(schedule_id) {
+      await service.getDeliveryScheduleDetails_DELIVERY_SCHEDULING_INVOICE_CHALLAN_PRINTING(schedule_id)
+        .then(res => {
+          this.SCHEDULE_DETAILS_LIST = []
+          this.SCHEDULE_DETAILS_LIST_CHEMIST = []
+          this.SCHEDULE_DETAILS_LIST_INSTITUTION = []
+          console.log(res.data)
+          this.SCHEDULE_DETAILS_LIST = res.data.schedule_list
+          if(res.data.schedule_list) {
+            for(let i=0; i<res.data.schedule_list.length; i++) {
+              if(res.data.schedule_list[i].customer_info.customer_type === '422') {
+                this.SCHEDULE_DETAILS_LIST_CHEMIST.push(res.data.schedule_list[i])
+              } else if(res.data.schedule_list[i].customer_info.customer_type === '424') {
+                this.SCHEDULE_DETAILS_LIST_INSTITUTION.push(res.data.schedule_list[i])
+              }
+            }
+          }
+        })
+    }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import url("./InvoiceChallanSummery.less");
+</style>
---
+++ src/views/private_views/SD/InvoiceChallanSummery/Sidebar/InvoiceChallanPrintingLeftList.vue
@@ -0,0 +1,153 @@
+<template>
+  <div id="invoice-challan-summery-left-list" class="invoice-challan-summery-left-list">
+    <div class="invoice-challan-summery-list-section">
+      <div class="invoice-challan-summery-list-section-inner">
+        <!-- <div class="month-selection-section">
+          <div class="month-selection-section-inner">
+            <div class="select-option-label">
+              <p class="month-selection-label">Status:</p>
+            </div>
+            <div class="select-options">
+              <span class="down-icon"><i class="fas fa-chevron-down"></i></span>
+              <select
+                title="Pick a DA"
+                class="selectpicker"
+                v-model="selectedDA"
+                @change="onChange()"
+              >
+                <option v-for="(da, m) in DSI_list" :key="m">
+                  {{ da.name }}
+                </option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div class="date-range">
+          <div class="date-range-inner">
+            <p>
+              <span>Date Range</span>
+              <date-picker v-model="range" lang="en" range type="date" format="YYYY-MM-DD" width="500"></date-picker>
+            </p>
+          </div>
+        </div> -->
+        <div class="location-title">
+          <div class="location-title-inner">
+            <p>DS Invoice List (<span>{{ INVOICE_LIST.length }}</span>)</p>
+          </div>
+        </div>
+        <div class="location-list-section">
+          <div class="location-list-section-inner">
+            <div
+              :id="'invoice-challan-summery-section-list-' + p"
+              class="invoice-challan-summery-section-list"
+              v-for="(invoice, p) in INVOICE_LIST"
+              :key="p"
+              @click="invoiceClickHandler(invoice, p)"
+            >
+              <div class="invoice-challan-summery-section-list-inner">
+                <div class="name-status-section">
+                  <div class="name-section">
+                    <p class="name-text">DS{{ invoice.id }}</p>
+                  </div>
+                  <div class="status-section">
+                    <p class="status-text">
+                      {{ (invoice.schedule_date).split(' ')[0] }}
+                    </p>
+                  </div>
+                </div>
+                <div class="contact-number-section">
+                  <p class="contact-number jmi-txt-nowrap-ellipsis-middle_60">{{ invoice.da_info.name }}</p>
+                  <p class="status"><span class="status-icon"></span>Initial Phase</p>
+                </div>
+                <!-- <div class="territory-text-section">
+                  <p class="territory-text">{{ invoice }}</p>
+                </div> -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Service from "../../../../../service/ERPSidebarService";
+const service = new Service();
+// import HotelDatePicker from 'vue-hotel-datepicker'
+// import DatePicker from 'vue2-datepicker'
+
+export default {
+  components: {
+    // HotelDatePicker,
+    // DatePicker
+  },
+  data() {
+    return {
+      selectedDA: null,
+      DSI_list: [
+        {
+
+        }
+      ],
+      locations_details_list: [],
+      // minNights: 2,
+      // maxNights: 30,
+      // disabledDaysOfWeek: ['Sunday'],
+      // showCloseButton: true
+      // date: '',
+      // time: '',
+      // timePickerOptions: {
+      //   start: '00:00',
+      //   step: '00:30',
+      //   end: '23:30'
+      // },
+      // datetime: '',
+      // range: ''
+      INVOICE_LIST: []
+    };
+  },
+  created() {
+    service.getDAlistForDeliverySchedule().then((res) => {
+      this.DSI_list = res.data;
+    });
+
+    service.getInvoiceChallanListForDeliverySchedule().then((res) => {
+      this.locations_details_list = res.data;
+    });
+  },
+  async mounted() {
+    await this.DELIVERY_SCHEDULE_LIST__FROM_SERVICE()
+  },
+  methods: {
+    onChange() {
+      console.log(this.selectedDA);
+    },
+    invoiceClickHandler(invoice, index) {
+            let length = document.getElementsByClassName('invoice-challan-summery-section-list').length
+            for(let i=0; i<length; i++) {
+                document.querySelector('#invoice-challan-summery-section-list-' + i).className = 'invoice-challan-summery-section-list'
+            }
+            if(document.querySelector('#invoice-challan-summery-section-list-' + index).className === 'invoice-challan-summery-section-list') {
+                document.querySelector('#invoice-challan-summery-section-list-' + index).className = 'invoice-challan-summery-section-list active'
+            } else {
+                document.querySelector('#invoice-challan-summery-section-list-' + index).className = 'invoice-challan-summery-section-list'
+            }
+      this.$emit('invoice_id_from_left', invoice.id)
+    },
+    // --------------------------------------------------------------------------------------
+    // Service CALL
+    async DELIVERY_SCHEDULE_LIST__FROM_SERVICE() {
+      service.getDeliveryScheduleList_DELIVERY_SCHEDULING_INVOICE_CHALLAN_PRINTING()
+        .then(res => {
+          console.log(res.data)
+          this.INVOICE_LIST = res.data.schedule_list
+        })
+    }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import url("./InvoiceChallanPrintingLeftList.less");
+</style>
